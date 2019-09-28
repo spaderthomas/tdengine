8f41263bf47218122b0128a64ca61ea7c7a0c231
Thomas <thomas.spader@gmail.com>
2019-09-28 14:31:15 -0400

    some more entities

--- src/script_to_def.hpp
+++ src/script_to_def.hpp
@@ -44,5 +44,41 @@
 	{
 		"characters/wilson/scenes/main1.tds",
 		{"wilson"}
+	},
+	{
+		"cutscenes/test/trigger.tds",
+		{"CT_test"}
+	},
+	{
+		"props/boon_bed.tds",
+		{"boon_bed"}
+	},
+	{
+		"props/boon_nightstand.tds",
+		{"boon_nightstand"}
+	},
+	{
+		"props/boon_rug.tds",
+		{"boon_rug"}
+	},
+	{
+		"props/downstairs.tds",
+		{"downstairs"}
+	},
+	{
+		"props/fences.tds",
+		{"fence_left", "fence_middle", "fence_right", "fence_post", "fence_top"}
+	},
+	{
+		"props/foliage.tds",
+		{"tree", "cactus", "bush"}
+	},
+	{
+		"props/upstairs.tds",
+		{"upstairs"}
+	},
+	{
+		"utils/tiles.tds",
+		{"border_bottom_left_grass", "border_bottom_left_sand_grass", "border_bottom_right_grass", "border_bottom_right_sand_grass", "border_bottom_sand_grass", "border_left_sand_grass", "border_right_sand_grass", "border_top_left_grass", "border_top_left_sand_grass", "border_top_right_grass", "border_top_right_sand_grass", "border_top_sand_grass", "cobblestone1", "cobblestone2", "cobblestone3", "cobblestone4", "grass", "grass_flower1", "grass_flower2", "grass_flower3", "overworld_ground1", "sand", "sand_cracked", "wood_floor", "sand_path_bottom", "sand_path_bottomleft_inner", "sand_path_bottomleft_outer", "sand_path_bottomright_inner", "sand_path_bottomright_outer", "sand_path_left", "sand_path_right", "sand_path_top", "sand_path_topleft_inner", "sand_path_topleft_outer", "sand_path_topright_inner", "sand_path_topright_outer"}
 	}
 };
